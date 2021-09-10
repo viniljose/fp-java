2587a0e91dc3530d8d03ba73d92e7c1de464b66e
viniljose <vinilpj@gmail.com>
2021-09-10 13:47:06 +0530

    Consumer Iteration

--- src/main/java/com/fp/Iteration.java
+++ src/main/java/com/fp/Iteration.java
@@ -2,6 +2,7 @@ package com.fp;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class Iteration {
     final List<String> friends =
@@ -10,12 +11,24 @@ public class Iteration {
     public static void main(String[] args) {
         Iteration iteration = new Iteration();
         iteration.oldWay();
+        iteration.consumer();
     }
 
     //Old Way
     public void oldWay() {
+        System.out.println("---Old Way---");
         for (String name : friends) {
             System.out.println(name);
         }
+    }
+
+    public void consumer(){
+        System.out.println("---Consumer---");
+        friends.forEach(new Consumer<String>() {
+            @Override
+            public void accept(String name) {
+                System.out.println(name);
+            }
+        });
     }
 }

8325fc4b91f5775010f21d9a2975fa1e4d0792b8
viniljose <vinilpj@gmail.com>
2021-09-21 17:42:16 +0530

    Create CalculateNAV.java

---
+++ src/main/java/com/fp/CalculateNAV.java
@@ -0,0 +1,28 @@
+package com.fp;
+
+import java.math.BigDecimal;
+import java.util.function.Function;
+
+/**
+ * Performs financial calculations with data from a web service.
+ */
+public class CalculateNAV {
+
+    private Function<String, BigDecimal> priceFinder;
+
+
+    public CalculateNAV(final Function<String, BigDecimal> aPriceFinder) {
+        priceFinder = aPriceFinder;
+    }
+
+    public BigDecimal computeStockWorth(
+            final String ticker, final int shares) {
+        return priceFinder.apply(ticker).multiply(BigDecimal.valueOf(shares));
+    }
+
+    public static void main(String[] args) {
+        CalculateNAV calculateNAV = new CalculateNAV(ticker -> new BigDecimal("6.01"));
+        System.out.println(calculateNAV.computeStockWorth("ABCD",1000));
+    }
+
+}

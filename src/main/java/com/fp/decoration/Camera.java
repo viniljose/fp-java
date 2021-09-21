d4b579d699c3a50687869467c0a6726c6aa92903
viniljose <vinilpj@gmail.com>
2021-09-21 21:44:08 +0530

    Create Camera.java

---
+++ src/main/java/com/fp/decoration/Camera.java
@@ -0,0 +1,34 @@
+package com.fp.decoration;
+
+import java.awt.*;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+@SuppressWarnings("unchecked")
+public class Camera {
+    private Function<Color, Color> filter;
+
+    public Camera(){
+        setFilters();
+    }
+
+    public Color capture(final Color input){
+        final Color processedColor = filter.apply(input);
+        return processedColor;
+    }
+
+    public void setFilters(final Function<Color,Color>... filters){
+        filter = Stream.of(filters)
+                .reduce((filter,next)->filter.compose(next))
+                .orElse(color -> color);
+    }
+
+    public static void main(String[] args) {
+        final Camera camera = new Camera();
+        final Consumer<String> printCaptured = (filterInfo) ->
+  System.out.println(String.format("With %s: %s", filterInfo,
+                camera.capture(new Color(200, 100, 200))));
+        printCaptured.accept("No Filter");
+    }
+}

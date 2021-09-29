427a9e48addd7b8003b30e3c5a7d894c269a7545
viniljose <vinilpj@gmail.com>
2021-09-29 09:26:22 +0530

    Create Holder.java

---
+++ src/main/java/com/fp/lazyinitialisation/Holder.java
@@ -0,0 +1,33 @@
+package com.fp.lazyinitialisation;
+
+import java.util.function.Supplier;
+
+/**
+ * When an instance of Holder is created, as we can see, an instance of Heavy is not created.
+ * This design achieves the goal of lazy initialization.
+ * This is a simple, lightweight implementation of the virtual proxy pattern
+ */
+public class Holder {
+    private Supplier<Heavy> heavySupplier = ()-> createHeavy();
+
+    public Holder() {
+        System.out.println("Holder Created");
+    }
+
+    public Heavy getHeavy(){
+        return heavySupplier.get();
+    }
+
+    private synchronized Heavy createHeavy(){
+        class HeavyFactory implements Supplier<Heavy> {
+            private final Heavy heavy = new Heavy();
+            public Heavy get(){return heavy;}
+        }
+
+        if(!HeavyFactory.class.isInstance(heavySupplier)){
+            heavySupplier = new HeavyFactory();
+        }
+
+        return heavySupplier.get();
+    }
+}

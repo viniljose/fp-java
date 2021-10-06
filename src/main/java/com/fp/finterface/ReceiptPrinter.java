db77d61bd33fb63ba5c109ac21a38d67792a11d5
viniljose <vinilpj@gmail.com>
2021-10-06 21:44:49 +0530

    Create ReceiptPrinter.java

---
+++ src/main/java/com/fp/finterface/ReceiptPrinter.java
@@ -0,0 +1,16 @@
+package com.fp.finterface;
+
+@FunctionalInterface
+public interface ReceiptPrinter {
+    void print(Receipt receipt);
+    private double getDiscountedPrice(Receipt receipt)
+    {
+        return receipt.price
+                - (receipt.price * receipt.discount);
+    }
+    default double computeTotal(Receipt receipt)
+    {
+        double discountedPrice = getDiscountedPrice(receipt);
+        return discountedPrice + (discountedPrice * receipt.tax);
+    }
+}

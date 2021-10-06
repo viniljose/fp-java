86df64e60240be62be73ef45334c1d97a0636dc3
viniljose <vinilpj@gmail.com>
2021-10-06 21:44:26 +0530

    Create Receipt.java

---
+++ src/main/java/com/fp/finterface/Receipt.java
@@ -0,0 +1,32 @@
+package com.fp.finterface;
+
+public class Receipt {
+    String item;
+    double price;
+    double discount;
+    double tax;
+    public Receipt(String i, double a, double d, double s)
+    {
+        item     = i;
+        price    = a;
+        discount = d;
+        tax      = s;
+    }
+    public Receipt(Receipt r)
+    {
+        item     = r.item;
+        price    = r.price;
+        discount = r.discount;
+        tax      = r.tax;
+    }
+
+    @Override
+    public String toString() {
+        return "Receipt{" +
+                "item='" + item + '\'' +
+                ", price=" + price +
+                ", discount=" + discount +
+                ", tax=" + tax +
+                '}';
+    }
+}
